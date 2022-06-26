669bc4f0997d955799d3891d1568f72794cb37ef
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-06-26 13:11:26 +0100

    Added two helper functions

--- test_main.py
+++ test_main.py
@@ -32,5 +32,29 @@ edge_cases_result = (
 )
 
 
+def convert_array_to_list(num_array: list) -> ListNode:
+    prev_node = None
+    start_node = None
+    for num in num_array:
+        new_node = ListNode(num)
+        if prev_node:
+            prev_node.next = new_node
+        else:
+            start_node = new_node
+        prev_node = new_node
+
+    return start_node
+
+
+def convert_list_to_array(node: ListNode) -> list:
+    num_array = []
+    current_node = node
+    while current_node:
+        num_array.append(current_node.val)
+        current_node = current_node.next
+
+    return num_array
+
+
 def test_add_two_numbers() -> None:
     assert False
