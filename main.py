2f3c9b9967ddba2b577198e729dca685ffeca0bb
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-06-26 13:22:37 +0100

    Implemented the actual function and changed a few test cases because I wrote them wrong initially.

--- main.py
+++ main.py
@@ -6,5 +6,49 @@ class ListNode:
 
 
 def add_two_numbers(l1: ListNode, l2: ListNode) -> ListNode:
-    """Returns the sum of the two linked lists"""
-    return ListNode()
+    result = None
+    current_node = None
+    carry = 0
+    while l1 is not None and l2 is not None:
+        s = l1.val + l2.val + carry
+        digit = s%10
+        new_node = ListNode(digit)
+        if current_node is not None:
+            current_node.next = new_node
+        if result is None:
+            result = new_node
+        current_node = new_node
+        carry = int(s/10)
+        l1 = l1.next
+        l2 = l2.next
+
+    while l1 is not None:
+        s = l1.val + carry
+        digit = s%10
+        new_node = ListNode(digit)
+        if current_node is not None:
+            current_node.next = new_node
+        if result is None:
+            result = new_node
+        current_node = new_node
+        carry = int(s/10)
+        l1 = l1.next
+
+    while l2 is not None:
+        s = l2.val + carry
+        digit = s%10
+        new_node = ListNode(digit)
+        if current_node is not None:
+            current_node.next = new_node
+        if result is None:
+            result = new_node
+        current_node = new_node
+        carry = int(s/10)
+        l2 = l2.next
+
+    if carry > 0:
+        new_node = ListNode(carry)
+        current_node.next = new_node
+
+
+    return result
