eb86295228f4e7615c3402186fca3e9636b92fd1
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-06-26 12:58:04 +0100

    Defined functions and classes, also defined use and edge cases.

---
+++ main.py
@@ -0,0 +1,10 @@
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def add_two_numbers(l1: ListNode, l2: ListNode) -> ListNode:
+    """Returns the sum of the two linked lists"""
+    return ListNode()
