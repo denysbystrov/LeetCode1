d240dc11d7efb5444f4a967a9bfb4d06dc9629ad
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-06-29 10:37:32 +0100

    implemented test for number 66

--- test_plus_one.py
+++ test_plus_one.py
@@ -13,5 +13,6 @@ edge_cases = (
 )
 
 
-def test_plus_one():
-    assert False
+@pytest.mark.parametrize(('input_x', 'expected'), use_cases+edge_cases)
+def test_plus_one(input_x, expected):
+    assert plus_one(input_x) == expected
