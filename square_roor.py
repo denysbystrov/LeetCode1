c195a15621e6da707fb0f7b430a67fdf5db0ede2
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-06-29 11:16:30 +0100

    implemented number 69

--- square_roor.py
+++ square_roor.py
@@ -1,3 +1,14 @@
 
 def my_sqrt(x: int) -> int:
-    return 0
+    start = 0
+    end = x
+    while start <= end:
+        n = int((start+end)/2)
+        square_n = n*n
+        if square_n == x:
+            return n
+        elif square_n > x:
+            end = n-1
+        else:
+            start = n+1
+    return end
