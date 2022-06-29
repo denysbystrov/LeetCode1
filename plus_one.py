969c2ca4e0c153203d62de06f9b48e79b0612e6b
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-06-29 10:53:03 +0100

    implemented number 66

--- plus_one.py
+++ plus_one.py
@@ -1,4 +1,16 @@
 
 
 def plus_one(digits: list) -> list:
-    return []
+    addition_carry = 1
+    idx = len(digits)-1
+    while addition_carry > 0 and idx >= 0:
+        s = digits[idx] + addition_carry
+        new_digit = s % 10
+        addition_carry = int(s / 10)
+        digits[idx] = new_digit
+        idx = idx-1
+
+    if addition_carry == 1:
+        digits.insert(0, 1)
+
+    return digits
