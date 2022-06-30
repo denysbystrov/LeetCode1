c9c9b27a3269a60bd332c21058854ea9b05dd3ca
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-06-30 08:36:07 +0100

    First implementation for number 70, it is O(n^2), can probably do better.

--- climbing_stairs.py
+++ climbing_stairs.py
@@ -1,5 +1,14 @@
 """Number 70: Climbing Stairs"""
+import math
 
 
 def climb_stairs(n: int) -> int:
-    return 0
+    num_twos = int(n/2)
+    total_ways = 0
+    for i in range(1, num_twos+1):
+        j = n - 2*i
+        combinations = math.factorial(i+j)/(math.factorial(i)*math.factorial(j))
+        print(combinations)
+        total_ways += combinations
+
+    return total_ways+1
