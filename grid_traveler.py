d7997e5a4c58badfef50b8bb3111df1e526087d3
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-07-01 12:07:06 +0100

    Small updates for grid traveller problem

--- grid_traveler.py
+++ grid_traveler.py
@@ -3,16 +3,15 @@
 
 
 def grid_traveller(m: int, n: int, memo={}) -> int:
-    if m == 0 or n == 0:
-        return 0
-    elif m == 1 or n == 1:
+    if m == 1 or n == 1:
         return 1
 
-    key = str(m)+str(n)
+    key = str(m)+','+str(n) if m < n else str(n)+','+str(m)
+
     if key not in memo:
         memo[key] = grid_traveller(m-1, n, memo) + grid_traveller(m, n-1, memo)
 
     return memo[key]
 
 
-print(grid_traveller(2, 3))
+print(grid_traveller(18, 18))
