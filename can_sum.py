9f562ce110bbd6d48c9d9077fd7c46d5d1604ea9
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-07-01 16:37:55 +0100

    Now uses dynamic programming. Time complexity: O(n*m), space complexity O(m)

--- can_sum.py
+++ can_sum.py
@@ -2,17 +2,19 @@
 can be made by the numbers in the array. Return false if not"""
 
 
-def can_sum(target_sum: int, numbers: list) -> bool:
+def can_sum(target_sum: int, numbers: list, memo={}) -> bool:
     if target_sum == 0:
         return True
     if target_sum < 0:
         return False
+    if target_sum in memo:
+        return memo[target_sum]
 
     for num in numbers:
-        if can_sum(target_sum-num, numbers):
+        if can_sum(target_sum-num, numbers, memo):
+            memo[target_sum] = True
             return True
 
+    memo[target_sum] = False
     return False
-
-
 
