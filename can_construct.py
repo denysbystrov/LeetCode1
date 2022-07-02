892773ae466f05398951ec7fff72c380bb469a27
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-07-02 13:00:36 +0100

    Implemented can construct function

--- can_construct.py
+++ can_construct.py
@@ -2,4 +2,27 @@ from typing import List
 
 
 def can_construct(target_string: str, components: List[str]) -> bool:
-    return False
+    def check_component(c, i):
+        if i + len(c) > len(target_string):
+            return False
+        for j in range(len(c)):
+            if c[j] != target_string[i+j]:
+                return False
+        return True
+
+    def can_construct_rec(i):
+        if i > len(target_string):
+            return False
+        if i == len(target_string):
+            return True
+
+        for c in components:
+            if check_component(c, i) and can_construct_rec(i+len(c)):
+                return True
+
+        return False
+
+    return can_construct_rec(0)
+
+
+print(can_construct('ab', ['a', 'b']))
