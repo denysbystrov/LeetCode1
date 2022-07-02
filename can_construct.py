e3131d9dd8a427b7e35bde0a1fc07ce7a5939032
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-07-02 16:01:20 +0100

    Implemented can construct function with dynamic programing

--- can_construct.py
+++ can_construct.py
@@ -10,19 +10,23 @@ def can_construct(target_string: str, components: List[str]) -> bool:
                 return False
         return True
 
-    def can_construct_rec(i):
+    def can_construct_rec(i, memo):
+        if i in memo:
+            return memo[i]
         if i > len(target_string):
             return False
         if i == len(target_string):
             return True
 
         for c in components:
-            if check_component(c, i) and can_construct_rec(i+len(c)):
+            if check_component(c, i) and can_construct_rec(i+len(c), memo):
+                memo[i] = True
                 return True
 
+        memo[i] = False
         return False
 
-    return can_construct_rec(0)
+    return can_construct_rec(0, {})
 
 
 print(can_construct('ab', ['a', 'b']))
