0831b70e3dccc8bfb902319b01d1c2acabd9f3b5
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-07-02 21:18:01 +0100

    Implemented tests for number 88

---
+++ merge_lists.py
@@ -0,0 +1,6 @@
+"""Number 88: merge two sorted lists"""
+from typing import List
+
+
+def merge(nums1: List[int], m: int, nums2: List[int], n:int) -> None:
+    pass
---
+++ test_merge_lists.py
@@ -0,0 +1,21 @@
+import pytest
+from merge_lists import merge
+
+
+use_cases = (
+    ([1, 3, 5, 7], [2, 4, 6, 8], [1, 2, 3, 4, 5, 6, 7, 8]),
+    ([1, 2, 3], [4, 5, 6], [1, 2, 3, 4, 5, 6]),
+    ([4, 6, 9], [1, 2, 10], [1, 2, 4, 6, 9, 10]),
+)
+
+edge_cases = (
+    ([1], [], [1]),
+    ([1, 1, 1], [1, 1, 2], [1, 1, 1, 1, 1, 2])
+)
+
+
+@pytest.mark.parametrize(('nums1', 'nums2', 'expected'), use_cases+edge_cases)
+def test_merge(nums1, nums2, expected):
+    result = nums1
+    merge(result, len(nums1), nums2, len(nums2))
+    assert result == expected
