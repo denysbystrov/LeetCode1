de36c3b3979100621f82955968f1d74e03979f32
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-07-04 09:05:51 +0100

    Implemented count construct problem using recursion

--- count_construct.py
+++ count_construct.py
@@ -4,4 +4,29 @@ from typing import List
 
 
 def count_construct(target_sring: str, word_bank: List[str]) -> int:
-    return 0
+
+    def word_is_prefix(word, i):
+        if len(word) + i > len(target_sring):
+            return False
+
+        for j in range(0, len(word)):
+            if word[j] != target_sring[i+j]:
+                return False
+        return True
+
+    def count_construct_rec(i):
+        if i == len(target_sring):
+            return 1
+        if i > len(target_sring):
+            return 0
+
+        total = 0
+        for w in word_bank:
+            if word_is_prefix(w, i):
+                total += count_construct_rec(i+len(w))
+
+        return total
+
+    return count_construct_rec(0)
+
+
